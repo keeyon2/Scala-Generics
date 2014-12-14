0ccafce33a54ce7d81f543dd83a590ec93ea2fc6
Keeyon Ebrahimi <keeyon2@gmail.com>
2014-12-14 18:35:41 -0500

    Scala Abstract Ordered Trees Done

--- Part2.scala
+++ Part2.scala
@@ -1,17 +1,9 @@
-import scala.math;
-
 case class OInt (i: Int) extends Ordered[OInt] {
   val data: Int = i;
 
-  //def compare(that: OInt) = this.data - that.data;
   def compare(otherOInt: OInt) = otherOInt match {
     case OInt(otherI) => i compare otherI
   }
-}
-
-class KInt (i: Int) {
-  val data: Int = i;
-
 }
 
 // Definition of what the Abstract Class it
@@ -22,41 +14,23 @@ case class ONode[T <: Ordered[T]] (ls: List[OTree[T]]) extends OTree[T]
   def compare(otherTree: OTree[T]) = otherTree match {
     case OLeaf(_) => 1;
     case ONode(x::xs) =>
-      var currentCompare: Int = ls.head compare x;
-      if ( currentCompare == 0)
+      var currentCompare: Int = 0;
+      if (ls.isEmpty)
       {
-        currentCompare = ONode(ls.tail) compare ONode(xs);
-      }
-      currentCompare;
-    /*case ONode(other_List) => 
-      val arrayLength: Int = math.abs(ls.length - other_List.length);
-      var comparingInt: Int = 0;
-      for (x <- 0 to arrayLength - 1)
-      {
-        comparingInt = ls(x) compare other_List(x);
-        if (comparingInt != 0)
-        {
-          return comparingInt;
-        }
-      }
-      if (ls.length > other_List.length)
-      {
-        return 1;
-      }
-      else if(ls.length < other_List.length)
-      {
-        return -1;
+        currentCompare = -1;
       }
       else
       {
-        return 0;
+        currentCompare = ls.head compare x;
       }
-      val headCompare: Int = ls.head compare other_List.head;
-      if (headCompare == 0)
+      if ( currentCompare == 0)
       {
-        headCompare = ls(1) compare 
+        if (!(ls.tail.isEmpty && xs.isEmpty))
+        {
+          currentCompare = ONode(ls.tail) compare ONode(xs);
+        }
       }
-      headCompare*/
+      currentCompare;
     case _ => 1
   }
 }
@@ -72,12 +46,62 @@ case class OLeaf[T <: Ordered[T]] (i: T) extends OTree[T]
 
 object Part2 {
   def main(args: Array[String]) = {
-    //println("BUDDY");
-    var foo = (new OInt(5)).compare(new OInt(6));
-    println("Should return -1: " + foo);
+    test();
   }
   def compareTrees[T](Tree1: OTree[T], Tree2: OTree[T])
   {
-    println("OH HI");
+    val testInt: Int = Tree1 compare Tree2;
+    if (testInt < 0)
+    {
+      println("Less");
+    }
+    else if (testInt > 0)
+    {
+      println("Greater");
+    }
+    else
+    {
+      println("Equal");
+    }
+  }
+  def test() {
+
+    val tree1 = ONode(List(OLeaf(new OInt(6))))
+
+    val tree2 = ONode(List(OLeaf(new OInt(3)),
+         OLeaf(new OInt(4)), 
+         ONode(List(OLeaf(new OInt(5)))), 
+         ONode(List(OLeaf(new OInt(6)), 
+              OLeaf(new OInt(7))))));
+
+    val treeTree1: OTree[OTree[OInt]] = 
+      ONode(List(OLeaf(OLeaf(new OInt(1)))))
+
+    val treeTree2: OTree[OTree[OInt]] = 
+      ONode(List(OLeaf(OLeaf(new OInt(1))),
+     OLeaf(ONode(List(OLeaf(new OInt(2)), 
+          OLeaf(new OInt(2)))))))
+
+
+    print("tree1: ")
+    println(tree1)
+    print("tree2: ")
+    println(tree2)
+    print("treeTree1: ")
+    println(treeTree1)
+    print("treeTree2: ")
+    println(treeTree2)
+    print("Comparing tree1 and tree2: ")
+    compareTrees(tree1, tree2)
+    print("Comparing tree2 and tree2: ")
+    compareTrees(tree2, tree2)
+    print("Comparing tree2 and tree1: ")
+    compareTrees(tree2, tree1)
+    print("Comparing treeTree1 and treeTree2: ")
+    compareTrees(treeTree1, treeTree2)
+    print("Comparing treeTree2 and treeTree2: ")
+    compareTrees(treeTree2, treeTree2)
+    print("Comparing treeTree2 and treeTree1: ")
+    compareTrees(treeTree2, treeTree1)
   }
 }
